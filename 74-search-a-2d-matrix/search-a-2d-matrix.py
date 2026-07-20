df26f2b3c8a3321d7a8e93eac49f7d23579ca1e9
Chetan Sonigara <chetansonigara01@gmail.com>
2026-07-21 04:36:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (12.10%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,16 +1,31 @@
-# Brute Force Code
+# Optimal Code
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # traverse each row of the matrix
-        for row in matrix:
-            # traverse every element in the current row
-            for num in row:
-                # if the target element is found return true immediately
-                if num == target:
-                    return True
-        # target is not present in the entire matrix
+        # store the number of rows and columns
+        rows = len(matrix)
+        cols = len(matrix[0])
+        # treat the matrix as virtual 1D sorted array intialize the binary search boundaries
+        left = 0
+        right = rows * cols - 1
+        # perfrom binary search
+        while left <= right:
+            # find the middle index
+            mid = left + (right - left) // 2
+            # convert the virtual 1D index into the corresponding 2D coorfinates
+            row = mid // cols
+            col = mid % cols
+            # if the middle element is greater than the target search the left half
+            if matrix[row][col] > target:
+                right = mid - 1
+            # if the middle element is smaller than the target search the right half
+            elif matrix[row][col] < target:
+                left = mid + 1
+            # target found
+            else:
+                return True
+        # target does not exists in the matrix
         return False
 
-# Time Complexity : O(N^2)
-# Space Complexity : O(1) 
+# Time Complexity : O(Nlog)
+# Space Complexity : O(1)
         
