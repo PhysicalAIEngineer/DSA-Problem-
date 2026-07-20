bba6cfada7cd67b6b8466f2b347918c27d181883
Chetan Sonigara <chetansonigara01@gmail.com>
2026-07-21 04:21:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.43%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,16 +1,27 @@
-# Brute Force Code
+# Optimal Code
 class Solution:
+    # find the minimum element (pivot) in the rotated sorted array
+    def findPivot(self, nums: list[int]):
+        # intialize the binary search boundaries
+        left = 0
+        right = len(nums) - 1
+        # perform binary search
+        while left < right:
+            # find the middle index
+            mid = left + (right - left) // 2
+            # if the middle element is greater than the  rightmost element the minimum must be in the right half
+            if nums[mid] > nums[right]:
+                # discard the left half including mid
+                left = mid + 1
+            # otherwise the minimum is at mid or somewhere in the left half
+            else:
+                right = mid
+        # left pointer now points to the minium element
+        return nums[left]
+    # return the minimum element in the rotated sorted array 
     def findMin(self, nums: List[int]) -> int:
-        # assume the first element is the minimum values
-        minimum = nums[0]
-        # traverse the entire array
-        for i in range(len(nums)):
-            # if the current element is smallers than the stored minimum update the minimum values
-            if nums[i] < minimum:
-                minimum = nums[i]
-        # return the smallest element in the array
-        return minimum 
-
-# Time Complexity : O(N)
-# Space Complexity : O(1)
+        # find and return the pivot element
+        return self.findPivot(nums)
         
+# Time Complexity : O(Nlog)
+# Space Complexity : O(1)
