5efbe58fa9aef60fea4aa0d842dbcb4a2395877d
Chetan Sonigara <chetansonigara01@gmail.com>
2026-08-04 18:34:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (14.48%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,24 +1,75 @@
-# Brute Force Code
+# Optimal Code
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # combine both arrays into one array
-        merged = nums1 + nums2
-        # sort the combined array this makes it easy to find the middle element
-        merged.sort()
-        # total number of element 
-        n = len(merged)
-        # if the total number of element is odd there is exactly one middle element
-        if n % 2 == 1:
-            # middle index is n // 2
-            return float(merged[n // 2])
-        # if the total number of element is even there are two middle eleement
-        else:
-            # index of the left middle element
-            middle1 = merged[(n // 2) - 1]
-            # index of the right middle element
-            middle2 = merged[n // 2]
-        # median is the average of the two middle  element
-        return (middle1 + middle2) / 2
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        # store the number of elements in nums1
+        m = len(nums1)
+        # store the number of elements in nums2
+        n = len(nums2)
+        # total number of elements if both arrays were combined
+        size = m + n
+        # find the two middle positions
+        idx1 = (size // 2) - 1
+        idx2 = size // 2
+        # variables to store the two middle elements
+        element1 = -1
+        element2 = -1
+        # pointer for nums1
+        i = 0
+        # pointer for nums2
+        j = 0
+        # represents the current position in the imaginary merged array
+        k = 0
+        # compare elements from both arrays and process them in sorted order
+        while i < m and j < n:
+            # if nums1 has the smaller element take the element from nums1
+            if nums1[i] < nums2[j]:
+                # if this is the first middle position store the element
+                if k == idx1:
+                    element1 = nums1[i]
+                # if this is the second middle position store the element
+                if k == idx2:
+                    element2 = nums1[i]
+                # move nums1 pointer forward
+                i += 1
+            else:
+                # otherwise, take the element from nums2
+                if k == idx1:
+                    element1 = nums2[j]
+                # if this is the second middle position store the element
+                if k == idx2:
+                    element2 = nums2[j]
+                # move nums2 pointer forward
+                j += 1
+            # move to the next position in the imaginary merged array
+            k += 1
+        # if nums2 is exhausted process the remaining elements of nums1
+        while i < m:
+            # check if the current element is one of the middle elements
+            if k == idx1:
+                element1 = nums1[i]
+            if k == idx2:
+                element2 = nums1[i]
+            # move nums1 pointer forward
+            i += 1
+            # move to the next merged position
+            k += 1
+        # if nums1 is exhausted process the remaining elements of nums2
+        while j < n:
+            # check if the current element is one of the middle elements
+            if k == idx1:
+                element1 = nums2[j]
+            if k == idx2:
+                element2 = nums2[j]
+            # move nums2 pointer forward
+            j += 1
+            # move to the next merged position
+            k += 1
+        # if the total number of elements is odd there is only one middle element. element2 contains that middle element.
+        if size % 2 == 1:
+            return element2
+        # if the total number of elements is even there are two middle elements.
+        # median = average of the two middle elements.
+        return (element1 + element2) / 2.0
 
 # Time Complexity : O(N)
 # Space Complexity : O(N)
