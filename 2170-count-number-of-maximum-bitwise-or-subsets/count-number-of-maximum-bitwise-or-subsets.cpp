879edc68fbe9a0e056d8cf8f8e132e93a47da58e
Chetan Sonigara <chetansonigara01@gmail.com>
2026-08-12 01:41:44 +0530

    Time: 123 ms (13.55%) | Memory: 313.5 MB (7.41%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,56 +1,53 @@
-// Brute Force Code
+// Optimal Code
 class Solution {
-private:
-    // store the maximum bitwise or found so far. initially, no elements have been selected so the or value starts at 0.
-    int maximum_or = 0;
-    // store the number of subsets that have the maximum OR value found so far
-    int count = 0;
-    // generate all possible subsets using backtracking at every element, we have two choices: 1. include the current element and 2. do not include the current element this generates every possible subset.
-    void backtrack(int index, int current_or, int chosen, const std::vector<int>& nums) {
-        // Base case : if index reaches the length of nums have processed every element therefore, the current subset is complete.
-        if (index == nums.size()) {
-            // ignore the empty subset chosen represents how many elements were included in the current subset.
-            if (chosen == 0) {
-                return;
-            }
-            // found a new maximum OR if the OR of the current subset is greater than the maximum OR seen so far
-            if (current_or > maximum_or) {
-                // update the maximum OR.
-                maximum_or = current_or;
-                // since this is the new maximum all previous subsets are no longer considered maximum current subset is the first subset having this new maximum OR.
-                count = 1;
-            // found another subset with same maximum if the current subset has exactly the same OR as the maximum OR
-            } else if (current_or == maximum_or) {
-                count += 1;
-            }
-            // current subset has been processed.
-            return;
-        }
-        // Choice 1: include nums[index]
-        // add nums[index] to the current subset.
-        // update the OR: current_or | nums[index]
-        // also increase chosen because selected one more element.
-        backtrack(index + 1, current_or | nums[index], chosen + 1, nums);
-        // Choice 2: Do not include nums[index]
-        // leave current_or unchanged because nums[index] is not selected chosen also remains unchanged.
-        backtrack(index + 1, current_or, chosen, nums);
-    }
-
 public:
-    int countMaxOrSubsets(std::vector<int>& nums) {
-        // store the maximum bitwise or found so far. initially, no elements have been selected so the or value starts at 0.
-        maximum_or = 0;
-        // store the number of subsets that have the maximum OR value found so far
-        count = 0;
-        // start generating subsets
-        // 1. index = 0  → start from the first element.
-        // 2. current_or = 0 → no elements have been selected yet.
-        // 3. chosen = 0 → The current subset is empty.
-        backtrack(0, 0, 0, nums);
-        // return the number of non-empty subsets whose bitwise OR is equal to the maximum OR.
-        return count;
+    // recursive function with memoization
+    // 1. idx     : current index in nums
+    // 2. curror  : bitwise OR of elements selected so far
+    // 3. nums    : input array
+    // 4. maxor   : maximum possible OR
+    // 5. t       : DP/memoization table
+    int countSubsets(int idx, int currOr, vector<int>& nums, int maxOr, vector<vector<int>>& t) {
+        // base case: all elements have been processed.
+        if(idx == nums.size()) {
+            // if current OR equals maximum OR this is one valid subset.
+            if(currOr == maxOr)
+                return t[idx][currOr] = 1; 
+            // otherwise, this subset is invalid.
+            return t[idx][currOr] = 0;
+        }
+        // check whether this state has already been calculated.
+        if(t[idx][currOr] != -1) {
+            return t[idx][currOr];
+        }
+        // choice 1: include nums[idx] in the subset.
+        int takeCount = countSubsets(idx+1, currOr | nums[idx], nums, maxOr, t);
+        // choice 2: do not include nums[idx].
+        int notTakeCount = countSubsets(idx+1, currOr, nums, maxOr, t);
+        // combine both choice total number of valid subset is : subset that take nums[idx] + subset that do not take nums[idx]
+        return t[idx][currOr] = takeCount + notTakeCount;
+    }
+    int countMaxOrSubsets(vector<int>& nums) {
+        // find the maximum possible OR the maximum OR is the OR of all elements.
+        int maxOr = 0;
+        // calculate or all element
+        for(int &num : nums) {
+            maxOr |= num;
+        }
+        // number of elements.
+        int n = nums.size();
+        // DP table:
+        // t[idx][curror]
+        // idx     -> current position
+        // curror  -> OR obtained so far
+        // -1 means the state has not been calculated yet.
+        vector<vector<int>> t(n+1, vector<int>(maxOr+1, -1));
+        // initially no elements have been selected so current OR is 0.
+        int currOr = 0;
+        // start recursion from index 0
+        return countSubsets(0, currOr, nums, maxOr, t);
     }
 };
 
-// Time Complexity : O(N!)
+// Time Complexity : O(N)
 // Space Complexity : O(N)
