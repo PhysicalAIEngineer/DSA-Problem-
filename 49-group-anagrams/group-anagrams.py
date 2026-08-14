bf5c3e9a3c656d08c87a85b8a963e90e1c048122
Chetan Sonigara <chetansonigara01@gmail.com>
2026-08-14 17:52:30 +0530

    Time: 11 ms (82.58%) | Memory: 22.1 MB (52.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,40 +1,24 @@
-# Optimal Code [Without Sorting]
+# Optimal Code [Using Sorting]
 class Solution:
-    # generate a canonical representation of string using characters frequencies
-    def generate(self, s: str):
-        # frequency of array for characters "a" to "z"
-        count = [0] * 26
-        # count the frequency of each chracters
-        for ch in s:
-            count[ord(ch) - ord("a")] += 1
-        # bulid the canonical string from the frequency array
-        new_string = []
-        for i in range(26):
-            # append each characters according to its frequency
-            if count[i] > 0:
-                new_string.append(chr(i + ord("a")) * count[i])
-        # return the canonical string
-        return "".join(new_string)
-    # group all anagrams togethers
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # dictionary to map : canonical string -> list of anagrams
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        # dictionary to map : sorted string -> list of anagrams
         mp = {}
-        # process every string
-        for s in strs:
-            # generate the canonical key
-            key = self.generate(s)
-            # create a new group if the key is not presents
+        # traverse every string
+        for string in strs:
+            # sort the characters of the string all anagrams produce the same sorted key
+            key = "".join(sorted(string))
+            # create a new group if the key does not already exits
             if key not in mp:
                 mp[key] = []
-            # add the original string to its anagrams groups
-            mp[key].append(s)
-        # store all grouped anagrams
+            # add the original string to its corresponding groups
+            mp[key].append(string)
+        # store all anagram groups
         result = []
-        # collect every groups from the dictionary
+        # collect every group from the dictionary
         for group in mp.values():
             result.append(group)
-        # return all anagram groups
-        return result 
+        # return all grouped anagrams
+        return result
 
-# Time Complexity : O(N)
+# Time Complexity : O(Nlog)
 # Space Complexity : O(N)
