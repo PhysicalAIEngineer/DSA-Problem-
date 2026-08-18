081fa651999d06876e7ffae3e33e6f7bc1ec0981
Chetan Sonigara <chetansonigara01@gmail.com>
2026-08-18 11:25:37 +0530

    Time: 1 ms (63.03%) | Memory: 19.4 MB (22.13%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,18 +1,22 @@
-# Brute Force Code
+# Optimal Code
 class Solution:
+    # recursive function to find the winner's index (0-based)
+    def findWinnerIdx(self, n: int, k: int) -> int:
+        # base case: if only one player is left its index is 0.
+        if n == 1:
+            return 0
+        # recursively find the winner's index when there are n - 1 players.
+        index = self.findWinnerIdx(n - 1, k)
+        # convert the winner's index from the smaller circle (n - 1 players) to the current circle of n players using the Josephus formula.
+        index = (index + k) % n
+        # return the winner's index for n players.
+        return index
+    # return the winner's number (1-based)
     def findTheWinner(self, n: int, k: int) -> int:
-        # create a list containing all players numbered from 1 to n
-        players = [i for i in range(1, n + 1)]
-        # start counting from the first players index 0
-        index = 0
-        # continue eliminating players until only one players remains
-        while len(players) > 1:
-            # move (k - 1) steps from the current position modulo is used to wrap around the circular list
-            index = (index + k - 1) % len(players)
-            # remove the players at the calculated index the next round automatically starts from the players immediately after the removed players
-            players.pop(index)
-        # only remaining player is the winner
-        return players[0]
+        # find the winner's index (0-based).
+        result_index = self.findWinnerIdx(n, k)
+        # convert the 0-based index into the required 1-based player number
+        return result_index + 1
 
 # Time Complexity : O(N)
-# Space Complexity : O(N)
+# Space Complexity : O(1)
