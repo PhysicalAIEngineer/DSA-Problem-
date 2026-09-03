efce0a724397542b1b325e9015cbb048e92fd630
Chetan Sonigara <chetansonigara01@gmail.com>
2026-09-03 05:56:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.67%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.py
+++ 832-binary-tree-pruning/binary-tree-pruning.py
@@ -1,34 +1,18 @@
-# Brute Force Code 
+# Optimal Code
 class Solution:
-    # check whether the given subtree contains at least one node with values 1
-    def checkone(self, root):
-        # if the subtree is empty it does not contain 1
-        if root is None:
-            return False
-        # if the current node is 1 found node with value 1
-        if root.val == 1:
-            return True
-        # search in the left and right subtrees if either side contains 1 return true
-        return self.checkone(root.left) or self.checkone(root.right)
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # if the current node is node there is nothing to prune
+        # if the current node is none there is nothing to prune
         if root is None:
             return None
-        # first recursively prune the right subtree
-        self.pruneTree(root.right)
-        # then recursively prune the left subtree      
-        self.pruneTree(root.left)
-        # check whehter the left subtree contain any node with value 1 if if does not contain 1 remove the entire left subtree
-        if not self.checkone(root.left):
-            root.left = None
-        # check whether the right subtree contain any node with value 1 if the does not contain 1 remove the entire right subtree
-        if not self.checkone(root.right):
-            root.right = None
-        # if the current node is 0 and has no children it does not contain any 1 so remove this node
+        # recursively prune the left substree the returned values becomes the new left child
+        root.left = self.pruneTree(root.left)
+        # recursively prune the right subtree the returned values becomes the new right child
+        root.right = self.pruneTree(root.right)
+        # if the current node is 0 and both children are none then this subtree does not contain any 1 so delete the current node by returning none
         if root.left is None and root.right is None and root.val == 0:
             return None
-        # keep the current node
-        return root 
+        # otherwise keep the current node
+        return root
 
-# Time Complexity : O(N^2)
-# Space Complexity : O(N) 
+# Time Complexity : O(N)
+# Space COmlexity : O(N)
