06c8395b9888b5dff094e2dd7737114869d1affc
Chetan Sonigara <chetansonigara01@gmail.com>
2026-09-26 12:44:28 +0530

    Time: 3 ms (86.42%) | Memory: 20.4 MB (70.83%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,31 +1,32 @@
-# Brute Force Code & Optimal Code
+# Brute Force Code & Optimal Code [DFS Method]
 class Solution:
-    def DFS(self, adj, u, visited):
+    def DFS(self, adj, vertices, visited):
         # mark the current node as visited
-        visited[u] = True
+        visited[vertices] = True
         # visit all possible neighbours
-        for v in range(self.n):
-            # if there is a connection between u and v not been visited 
-            if adj[u][v] == 1 and not visited[v]:
-                # continue DFS from neighbours v
-                self.DFS(adj, v, visited)
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        for neighbours in range(self.numberofcities):
+            # if there is a connection and the neighbour has not been visited
+            if adj[vertices][neighbours] == 1 and not visited[neighbours]: 
+                # continue DFS from the neighbour's vertex
+                self.DFS(adj, neighbours, visited) 
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
         # number of cities
-        self.n = len(isConnected)
-        # keep track of visited citied
-        visited = [False] * self.n
+        self.numberofcities = len(isConnected)
+        # keep track of visited cities
+        visited = [False] * self.numberofcities
         # count total number of provinces
-        count = 0
-        # check every city
-        for i in range(self.n):
-            # if this city is not visited it belongs to new provinces
+        counttotalnumberprovinces = 0
+        # check every city 
+        for i in range(self.numberofcities):
+            # if this city is not visited it belongs to a new province
             if not visited[i]:
-                # found one new provinces
-                count += 1
-                # visit all cities connected to this provices
-                self.DFS(isConnected, i, visited)
-        # return the total number of provinces 
-        return count 
+                # found one new province
+                counttotalnumberprovinces += 1
+                # visit all cities connected to this province
+                self.DFS(isConnected, i, visited) 
+        # return the total number of provinces
+        return counttotalnumberprovinces 
+
 
 # Time Complexity : O(N)
-# Space Complexity : O(N)        
+# Space Complexity : O(N)
