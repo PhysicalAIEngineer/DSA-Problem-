f762a865e7aa87cc59e2f587347758d6030cd561
Chetan Sonigara <chetansonigara01@gmail.com>
2026-09-26 23:22:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (39.56%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,34 +1,41 @@
-# Brute Force Code & Optimal Code [DFS Method]
-class Solution:  
-    def checkBipartiteDFS(self, adj, curr, color, currColor): 
-        # give the current node its color
+# Brute Force Code & Optimal Code [BFS Method]
+from collections import deque  
+class Solution: 
+    def checkBipartiteBFS(self, adj, curr, color, currColor): 
+        # give the starting node its color
         color[curr] = currColor  
-        # visit all adjacent nodes
-        for v in adj[curr]: 
-            # if adjacent node has the same color then graph cannot be bipartite
-            if color[v] == color[curr]: 
-                return False 
-            # if adjacent node is not colored yet
-            if color[v] == -1: 
-                # give the opposite color to the adjacent node 1 -> 0 &  0 -> 1
-                colorOfV = 1 - currColor 
-                # recursively check the remaining graph
-                if self.checkBipartiteDFS(adj, v, color, colorOfV) == False: 
+        # queue for BFS traversal
+        que = deque() 
+        que.append(curr) 
+        # continue BFS while queue is not empty
+        while que: 
+            # remove the front node from the queue
+            u = que.popleft() 
+            # visit all adjacent nodes of u
+            for v in adj[u]: 
+                # if adjacent node already has the same color then graph is not bipartite
+                if color[v] == color[u]: 
                     return False 
-        # no conflict found in this DFS
+                # if adjacent node is not colored yet
+                elif color[v] == -1: 
+                    # give opposite color to the adjacent node 1 -> 0 & 0 -> 1
+                    color[v] = 1 - color[u] 
+                    # add the node to queue for further BFS
+                    que.append(v) 
+        # no color conflict found in this component
         return True 
     def isBipartite(self, adj): 
         V = len(adj) 
-        # -1 means the node is not colored yet
+        # -1 means node is not colored yet
         color = [-1] * V 
-        # so 1 = red & 0 = green check every node because the graph can contain multiple disconnected components
-        for i in range(V):
-            # if node is not visited/colored start a new DFS from this node
-            if color[i] == -1:
-                # start coloring this component with color 1
-                if self.checkBipartiteDFS(adj, i, color, 1) == False: 
+        # so, 1 = red &  0 = green check every connected component because graph can be disconnected
+        for i in range(V): 
+            # if node is not colored start BFS from this node
+            if color[i] == -1: 
+                # start this component with color 1
+                if self.checkBipartiteBFS(adj, i, color, 1) == False: 
                     return False 
-        # all nodes are colored without any conflict
+        # all components are successfully 2-colored
         return True
 
 # Time Complexity : O(N)
