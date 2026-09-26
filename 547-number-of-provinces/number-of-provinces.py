3c0ab2b4d1dc709b3a74cd9e27697f1f60efb9a4
Chetan Sonigara <chetansonigara01@gmail.com>
2026-09-26 13:21:56 +0530

    Time: 7 ms (48.88%) | Memory: 20.2 MB (89.80%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,32 +1,45 @@
-# Brute Force Code & Optimal Code [DFS Method]
+# Brute Force Code & Optimal Code [BFS Method]
+from collections import deque
 class Solution:
-    def DFS(self, adj, vertices, visited):
-        # mark the current node as visited
+    def __init__(self):
+        # store total number of cities
+        self.totalnumberofcities = 0
+    def BFS(self, adj, vertices, visited):
+        # queue for BFS traversal
+        queue = deque()
+        # start BFS traversal
+        queue.append(vertices)
+        # mark starting city as visited 
         visited[vertices] = True
-        # visit all possible neighbours
-        for neighbours in range(self.numberofcities):
-            # if there is a connection and the neighbour has not been visited
-            if adj[vertices][neighbours] == 1 and not visited[neighbours]: 
-                # continue DFS from the neighbour's vertex
-                self.DFS(adj, neighbours, visited) 
-    def findCircleNum(self, isConnected: list[list[int]]) -> int:
+        # continue until queue becomes empty
+        while queue:
+            # remove one city from queue
+            current_vertex = queue.popleft()
+            # visit all possible neighbours
+            for neighbour in range(self.totalnumberofcities):
+                # if connected and the neighbour is not visited
+                if adj[current_vertex][neighbour] == 1 and not visited[neighbour]:
+                    # mark the NEIGHBOUR as visited
+                    visited[neighbour] = True
+                    # add the NEIGHBOUR to the queue
+                    queue.append(neighbour)
+    def findCircleNum(self, isConnected):
         # number of cities
-        self.numberofcities = len(isConnected)
+        self.totalnumberofcities = len(isConnected)
         # keep track of visited cities
-        visited = [False] * self.numberofcities
+        visited = [False] * self.totalnumberofcities
         # count total number of provinces
-        counttotalnumberprovinces = 0
-        # check every city 
-        for i in range(self.numberofcities):
-            # if this city is not visited it belongs to a new province
+        count_totalnumberprovinces = 0
+        # check every city
+        for i in range(self.totalnumberofcities):
+            # if city is not visited it belongs to a new province
             if not visited[i]:
-                # found one new province
-                counttotalnumberprovinces += 1
-                # visit all cities connected to this province
-                self.DFS(isConnected, i, visited) 
-        # return the total number of provinces
-        return counttotalnumberprovinces 
-
+                # visit all cities connected to this city
+                self.BFS(isConnected, i, visited)
+                # one complete connected province found
+                count_totalnumberprovinces += 1
+        # return total number of provinces
+        return count_totalnumberprovinces
 
 # Time Complexity : O(N)
 # Space Complexity : O(N)
