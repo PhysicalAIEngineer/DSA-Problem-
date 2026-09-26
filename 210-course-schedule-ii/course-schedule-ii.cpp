adae79892ee52e2fb783b26dcc1693a7fc4b430b
Chetan Sonigara <chetansonigara01@gmail.com>
2026-09-26 19:00:34 +0530

    Time: 3 ms (72.26%) | Memory: 18.6 MB (43.54%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,41 +1,57 @@
-// Brute Fore Code & Optimal Code [DFS Method]
+// Brute Fore Code & Optimal Code [BFS Method]
 class Solution {
 public:
-    // flag to indicate whether a cycle exists
-    bool hasCycle = false;
-    void DFS(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited, vector<int>& st, vector<bool>& inRecursion) {
-        // mark current node as visited
-        visited[u] = true;
-        // mark current node as part of current DFS path
-        inRecursion[u] = true;
-        // first process all neighbours of u then add u to stack
-        for (int v : adj[u]) {
-            // if v is already in the current DFS path then a cycle is present
-            if (inRecursion[v]) {
-                hasCycle = true;
-                return;
-            }
-            // if v is not visited, perform DFS
-            if (!visited[v]) {
-                DFS(adj, v, visited, st, inRecursion);
+    // using Kahn's Algorithm (BFS)
+    vector<int> topologicalSortCheck(unordered_map<int, vector<int>>& adj, int n, vector<int>& indegree) {
+        // queue stores nodes whose indegree becomes 0
+        queue<int> que;
+        // count how many courses we can process
+        int count = 0;
+        // store the valid course order
+        vector<int> result;
+        // add all courses having indegree = 0 these courses have no prerequisites
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                // add course to result
+                result.push_back(i);
+                // one course is processed
+                count++;
+                // add course to queue
+                que.push(i);
             }
         }
-        // add current node after all its neighbours are processed
-        st.push_back(u);
-        // DFS of u is completed so remove u from current recursion path
-        inRecursion[u] = false;
+        // BFS
+        while (!que.empty()) {
+            // take a course whose prerequisites are completed
+            int u = que.front();
+            que.pop();
+            // visit all courses dependent on u
+            for (int v : adj[u]) {
+                // one prerequisite of v is completed
+                indegree[v]--;
+                // if all prerequisites of v are completed
+                if (indegree[v] == 0) {
+                    // add v to the course order
+                    result.push_back(v);
+                    // one more course is processed
+                    count++;
+                    // process v later
+                    que.push(v);
+                }
+            }
+        }
+        // if all courses are not processed then a cycle is present
+        if (count != n) {
+            return {};
+        }
+        // return valid course order
+        return result;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // create adjacency list
+        // adjacency list
         unordered_map<int, vector<int>> adj;
-        // track whether each course has been visited
-        vector<bool> visited(numCourses, false);
-        // track nodes currently present in DFS recursion path
-        vector<bool> inRecursion(numCourses, false);
-        // reset cycle flag
-        hasCycle = false;
-        // stack to store DFS finishing order
-        vector<int> st;
+        // indegree[i] = number of prerequisites of course i
+        vector<int> indegree(numCourses, 0);
         // build the directed graph
         for (auto& vec : prerequisites) {
             // 'a' = course to be taken
@@ -44,25 +60,11 @@ public:
             int b = vec[1];
             // create edge: b ---> a
             adj[b].push_back(a);
+            // one prerequisite is going into course a
+            indegree[a]++;
         }
-        // perform DFS for every course because graph may have multiple components
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                DFS(adj, i, visited, st, inRecursion);
-            }
-        }
-        // Final topological ordering if cycle is present valid course order is not possible
-        if (hasCycle) {
-            return {};
-        }
-        // reverse DFS finishing order
-        vector<int> result;
-        while (!st.empty()) {
-            result.push_back(st.back());
-            st.pop_back();
-        }
-        // Return valid course order
-        return result;
+        // find the topological order if cycle is present, return []
+        return topologicalSortCheck(adj, numCourses, indegree);
     }
 };
 
